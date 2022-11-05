ae6c5018f3b2c184161a92e9e9686ea8370aa111
martin todorov <martin.todorov@flatrocktech.com>
2022-11-05 19:04:55 +0200

    pagination wip

---
+++ src/Components/Pagination/Pagination.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./Pagination.scss";
+
+export default function Pagination() {
+  const [pagination, setPagination] = useState({
+    totalItems: 18,
+    currentPage: 3,
+    itemsPerPage: 6,
+  });
+
+  function changeCurrentPage() {
+    setPagination({
+      ...pagination,
+      currentPage: 5,
+    });
+  }
+
+  function renderPages(pagination) {
+    const totalPages = Math.ceil(
+      pagination.totalItems / pagination.itemsPerPage
+    );
+    let content = [];
+    let firstVisiblePage = 1;
+    let lastVisiblePage = totalPages;
+    if (totalPages > 3) {
+      firstVisiblePage = pagination.currentPage - 1;
+      lastVisiblePage = pagination.currentPage + 1;
+    }
+    if (pagination.currentPage == 1) {
+      firstVisiblePage = 1;
+    }
+    if (pagination.currentPage == totalPages) {
+      lastVisiblePage = pagination.currentPage;
+    }
+
+    for (let i = firstVisiblePage; i <= lastVisiblePage; i++) {
+      content.push(<li onClick={alert(1)}>{i}</li>);
+    }
+
+    return content;
+  }
+
+  return (
+    <ul className="pagination">
+      <li>Prev</li>
+      {renderPages(pagination)}
+      <li>Next</li>
+    </ul>
+  );
+}
--- src/Pages/ApiTest/ApiTest.js
+++ src/Pages/ApiTest/ApiTest.js
@@ -1,37 +1,37 @@
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 import api from "../../Services/api";
 import Submenu from "../../Components/Navigation/submenu";
 import NavLink from "../../Components/Navigation/NavLink";
+import Pagination from "Components/Pagination/Pagination";
 
 const ApiTest = () => {
-    const [jokes, setJokes] = useState([]);
+  const [jokes, setJokes] = useState([]);
 
-    useEffect(() => {
-        api.get('/facts', { params: { page: 2   } })
-            .then(response => setJokes(response.data.data))
-            .catch(e => console.log(e));
-    }, [])
+  useEffect(() => {
+    api
+      .get("/facts", { params: { page: 2 } })
+      .then((response) => setJokes(response.data.data))
+      .catch((e) => console.log(e));
+  }, []);
 
-
-
-    return (
-       <div>
-           <h2>Jokes</h2>
-           <table>
-               <tbody>
-               <tr>
-                   <th>Fact</th>
-               </tr>
-               {jokes.map((joke, index) =>
-                   <tr key={index}>
-                       <td>{joke.fact}</td>
-                   </tr>
-               )}
-               </tbody>
-
-           </table>
-       </div>
-    )
+  return (
+    <div>
+      <h2>Jokes</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th>Fact</th>
+          </tr>
+          {jokes.map((joke, index) => (
+            <tr key={index}>
+              <td>{joke.fact}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination></Pagination>
+    </div>
+  );
 };
 
 export default ApiTest;
